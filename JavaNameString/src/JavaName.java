20a613e146b1ef2d8d793723087b3d3bf02e8829
Franz Rodgers <zbayside@gmail.com>
2020-02-23 09:22:40 -0800

    Pushed the Source code

---
+++ JavaNameString/src/JavaName.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+
+public class JavaName {
+
+	public static void main(String[] args) {
+		
+
+				String str;
+				Scanner in = new Scanner(System.in);
+				System.out.println("");
+				System.out.println("Enter your name please: ");
+				System.out.println("");
+				str = in.nextLine();
+				System.out.println("Hi " + str);
+					}
+		
+
+		
+	}
+
+
